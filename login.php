e3d83e74e3650573a2a12b33fe490c128476e839
Sabda Chalise <chalisesabda4@gmail.com>
2023-12-02 17:50:47 +0545

    aw

--- login.php
+++ login.php
@@ -1,35 +1,44 @@
 <?php
 session_start();
-$connection = mysqli_connect('localhost', 'websitesabda', 'From2062@1', 'websitesabda') or die('SORRY FAILED');
+$connection = mysqli_connect('localhost', 'root', 'From2062@1', 'websitesabda') or die('SORRY FAILED');
 
-$email = mysqli_real_escape_string($connection, $_POST['email']);
-$username = mysqli_real_escape_string($connection, $_POST['username']);
-$password = mysqli_real_escape_string($connection, $_POST['password']);
-$gender = mysqli_real_escape_string($connection, $_POST['gender']);
+// Ensure that the form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Sanitize and retrieve form data
+    $username = mysqli_real_escape_string($connection, $_POST['username']);
+    $email = mysqli_real_escape_string($connection, $_POST['email']);
+    $password = mysqli_real_escape_string($connection, $_POST['password']);
+    $gender = mysqli_real_escape_string($connection, $_POST['gender']);
 
-$sql = "SELECT * FROM `form` WHERE email = '$email'";
-$qry = mysqli_query($connection, $sql);
+    // Hash the password using password_hash
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-// Check if the query was successful
-if (!$qry) {
-    die('Query failed: ' . mysqli_error($connection));
-}
+    // Insert data into the database
+    $insert_query = "INSERT INTO `form` (username, email, password, gender) VALUES ('$username', '$email', '$hashed_password', '$gender')";
+    $result = mysqli_query($connection, $insert_query);
 
-// Check if a record was found
-$res = mysqli_fetch_assoc($qry);
-if (!$res) {
-    echo "Email not found";
-} else {
-    $d_email = $res['email'];
-    $d_password = $res['password'];
-    $d_gender = $res['gender'];
+    // Check if the query was successful
+    if ($result) {
+        // Check if a record was found
+        $select_query = "SELECT * FROM `form` WHERE email='$email'";
+        $select_result = mysqli_query($connection, $select_query);
 
-    // Verify the password using password_verify
-    if (password_verify($password, $d_password)) {
-        $_SESSION['login'] = $email;
-        header("location:succelog.php");
+        if ($select_result) {
+            $record = mysqli_fetch_assoc($select_result);
+
+            // Verify the password using password_verify
+            if ($record && password_verify($password, $record['password'])) {
+                // Record found and password verified
+                $_SESSION['login'] = $email;
+                header("location:succelog.php");
+            } else {
+                echo "Error: Unable to verify password";
+            }
+        } else {
+            echo "Error: Unable to retrieve record";
+        }
     } else {
-        echo "Wrong password";
+        echo "Error: Unable to insert data";
     }
 }
 
